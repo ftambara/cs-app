70436bcf92481ea0dc6d3d90409dbb8bb79537cf
ftambara <98750668+ftambara@users.noreply.github.com>
2024-05-11 17:10:42 -0300

    Add chapter 1 notes and exercises

---
+++ chapter1/hello-clang.s
@@ -0,0 +1,34 @@
+	.text
+	.file	"hello.c"
+	.globl	main                            # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	movabsq	$.L.str, %rdi
+	movb	$0, %al
+	callq	printf
+	xorl	%eax, %eax
+	popq	%rbp
+	.cfi_def_cfa %rsp, 8
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.L.str,@object                  # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"hello, world\n"
+	.size	.L.str, 14
+
+	.ident	"clang version 18.1.1 (Fedora 18.1.1-1.fc40)"
+	.section	".note.GNU-stack","",@progbits
+	.addrsig
+	.addrsig_sym printf
