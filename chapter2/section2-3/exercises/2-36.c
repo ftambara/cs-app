c021ae80335414e68f7e251f64fc0e3a85a06275
ftambara <98750668+ftambara@users.noreply.github.com>
2024-05-17 20:13:44 -0300

    Add solution to problem 2.36

---
+++ chapter2/section2-3/exercises/2-36.c
@@ -0,0 +1,17 @@
+/* For the case where data type int has 32 bits, devise a version of tmult_ok
+ * (Problem 2.35) that uses the 64-bit precision of data type int64_t, without
+ * using division. */
+
+#include <stdint.h>
+#include <stdio.h>
+
+int tmult_ok(int32_t x, int32_t y) {
+    int64_t prod = (int64_t)x*y;
+    return prod == (int64_t)(int32_t)prod;
+}
+
+int main(void){
+    printf("tmult_ok(INT32_MAX/2, INT32_MAX/2)=%d\n",
+            tmult_ok(INT32_MAX/2, INT32_MAX/2));
+    printf("tmult_ok(2, INT32_MAX/2)=%d\n", tmult_ok(2, INT32_MAX/2));
+}
