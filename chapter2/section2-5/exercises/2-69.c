f3538153aa80c2eb9a3fa4593adf31d5a967d804
ftambara <98750668+ftambara@users.noreply.github.com>
2024-05-25 19:39:15 -0300

    Add solution to problem 2.69

---
+++ chapter2/section2-5/exercises/2-69.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/* Write code for a function with the following prototype: */
+/*
+* Do rotating left shift. Assume 0 <= n < w
+* Examples when x = 0x12345678 and w = 32:
+*
+n=4 -> 0x23456781, n=20 -> 0x67812345
+*/
+unsigned rotate_left(unsigned x, int n);
+
+/* Your function should follow the bit-level integer coding rules (page 164). Be
+careful of the case n = 0. */
+
+unsigned rotate_left(unsigned x, int n) {
+    /* Mask with all 1s to the right of the n-th bit, inclusive */
+    unsigned w = 8*sizeof(unsigned);
+    unsigned mask = (unsigned)(~0) >> n;
+    unsigned right_part = x & mask;
+    unsigned left_part = x & (~mask);
+    /* Shift left part in two stages to avoid shifting by w when n == 0 */
+    return ((left_part >> (w - n - 1)) >> 1) | (right_part << n);
+}
+
+int main(void) {
+    printf("rotate_left(0x12345678, 4) == 0x23456781 = %d\n",
+            rotate_left(0x12345678, 4) == 0x23456781);
+    printf("rotate_left(0x12345678, 20) == 0x67812345 = %d\n",
+            rotate_left(0x12345678, 20) == 0x67812345);
+    printf("rotate_left(0x12345678, 0) == 0x12345678 = %d\n",
+            rotate_left(0x12345678, 0) == 0x12345678);
+    return 0;
+}
