1f94640eaad862ba88e120bc8451649708ab9c11
ftambara <98750668+ftambara@users.noreply.github.com>
2024-05-25 18:44:37 -0300

    Add solution to problem 2.68

---
+++ chapter2/section2-5/exercises/2-68.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* Write code for a function with the following prototype: */
+
+/* 
+ * Mask with least signficant n bits set to 1
+ * Examples: n = 6 --> 0x3F, n = 17 --> 0x1FFFF
+ * Assume 1 <= n <= w
+ */
+int lower_one_mask(int n);
+/* 
+ * Your function should follow the bit-level integer coding rules (page 164). Be
+ * careful of the case n = w.
+ */
+
+int lower_one_mask(int n) {
+    return ((1 << (n-1)) << 1) - 1;
+}
+
+int main(void) {
+    printf("lower_one_mask(6) == 0x3F = %d\n", lower_one_mask(6) == 0x3F);
+    printf("lower_one_mask(17) == 0x1FFFF = %d\n",
+            lower_one_mask(17) == 0x1FFFF);
+    printf("lower_one_mask(32) == 0xFFFFFFFF = %d\n",
+            lower_one_mask(32) == 0xFFFFFFFF);
+    printf("lower_one_mask(1) == 0x1 = %d\n", lower_one_mask(1) == 0x1);
+    return 0;
+}
